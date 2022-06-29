720198b7184b77b217a4a34e5332b992b3108978
Shahar <sshwartz@ironscales.com>
2022-06-29 10:06:47 +0300

    dialog cdn

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-require('officejs.dialogs')
 
 var mailboxItem;
 
